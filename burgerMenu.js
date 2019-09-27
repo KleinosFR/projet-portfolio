1fba7506040720206eb72f202e67ce34ec656f97
KleinosFR <kleinos@hotmail.com>
2019-09-27 15:18:27 +0200

    debug burger menu

--- burgerMenu.js
+++ burgerMenu.js
@@ -1,33 +1,47 @@
-// const burgerIcon = document.getElementById("burgerIcon");
 const burgerMenu = document.getElementById("burgerMenu");
 const burgerIconContainer = document.getElementById("burgerIconContainer");
 let isBurgerClosed = true;
-console.log(burgerIcon);
-console.log(burgerMenu);
 
 
+// defines actions to excute to close burger menu
+const closeBurger = () => {
+  burgerMenu.style.display = "none";
+  burgerIconContainer.innerHTML = '<img id="burgerIcon" src="images/menu_icon.png" alt="menu icon">'
+  isBurgerClosed = true;
+}
+
+
+// listens to events on the burger icon
 
 burgerIconContainer.addEventListener("click", () => {
-    if (isBurgerClosed){
+    let screenWidth = window.innerWidth;
+    console.log(screenWidth);
+    if (isBurgerClosed && screenWidth < 768){
       burgerMenu.style.display = "flex";
       burgerIconContainer.innerHTML = '<img id="burgerIcon" src="images/logo_insta_noir.png" alt="menu icon">'
       isBurgerClosed = false;
       console.log(isBurgerClosed);
     }
       else {
-        burgerMenu.style.display = "none";
-      burgerIconContainer.innerHTML = '<img id="burgerIcon" src="images/menu_icon.png" alt="menu icon">'
-      isBurgerClosed = true;
+      closeBurger();
       console.log(isBurgerClosed)
     }
     ;
 })
 
 burgerMenu.addEventListener("click", () => {
-  burgerMenu.style.display = "none";
-  burgerIconContainer.innerHTML = '<img id="burgerIcon" src="images/menu_icon.png" alt="menu icon">';
-  isBurgerClosed = true;
+  let screenWidth = window.innerWidth;
+  if (screenWidth < 768) {
+  closeBurger();
   console.log(isBurgerClosed)
+  }
 } )
 
+// displays menu or not, on window resize
+
+window.onresize = () => { if (window.innerWidth>768) {
+  burgerMenu.style.display = "flex";} 
+        else {
+        closeBurger();                            
+      }}
 
